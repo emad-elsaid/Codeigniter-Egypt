8886708f8bcd9b9ded0240f772c23f682bcbdd0a
Emad Elsaid <blazeeboy@gmail.com>
2009-03-21 19:47:16 +0200

    section application under dev

---
+++ system/application/views/apps/section manager/addaction.php
@@ -0,0 +1,17 @@
+<?php 
+$ci =& get_instance();
+$ci->load->library( 'gui' );
+
+$s = new Section();
+$s->parent_section = $ci->input->post( 'parent_section' );
+$s->name = $ci->input->post( 'name' );
+$s->sort = $ci->input->post( 'sort' );
+$s->view = $ci->input->post( 'view' );
+$s->add = $ci->input->post( 'add' );
+$s->edit = $ci->input->post( 'edit' );
+
+$p = new Section();
+$p->get_by_id( $s->parent_section );
+
+$p->attach_section( $s );
+redirect( $ci->app->app_url( 'View Sections' ) );
---
+++ system/application/views/apps/section manager/view.php
@@ -0,0 +1,74 @@
+<?php 
+$ci =& get_instance();
+$ci->load->library( 'gui' );
+
+$sections = new Section();
+
+foreach( $sections->all as $item )
+{
+	$item->e = anchor( 'Edit', $ci->app->app_url( 'edit' ) );
+	$item->d = anchor( 'Delete', $ci->app->app_url( 'edit' ) );
+	
+}
+
+function add( $id, $sort , $text='+' )
+{
+	$ci =& get_instance();
+	$hidden = array( 'parent_section'=>$id, 'sort'=>$sort );
+	return $ci->gui->tooltipbutton(
+			$text
+			,'New Section Info' 
+			,$ci->gui->form(
+					$ci->app->app_url( 'addaction' )
+					,array(
+							'Name :'=>$ci->gui->textbox( 'name' )
+							,'view'=>$ci->gui->textarea( 'view' )
+							,'add'=>$ci->gui->textarea( 'add' )
+							,'edit'=>$ci->gui->textarea( 'edit' )
+							,''=>$ci->gui->button( '', 'Submit', array('type'=>'submit') )
+					)
+					,''
+					,$hidden
+			)
+	);
+}
+function printS( $id )
+{
+	$ci =& get_instance();
+	$s = new Section();
+	$s->get_by_id( $id );
+
+	$s->e = anchor( $ci->app->app_url( 'edit/' ).$s->id, 'Edit' );
+	$s->d = anchor( $ci->app->app_url( 'delete/' ).$s->id,'Delete' );
+	
+	$output = "<li>";
+	$output .= $s->id .'|';
+	$output .= $s->name;
+	$output .= add( $id, 0, "add first child for {$s->name}" );
+	$output .= '|';
+	$output .= $s->e;
+	$output .= '|';
+	$output .= $s->d;
+	$c = new Section();
+	$c->where( 'parent_section',  $id );
+	$c->order_by( 'sort', 'asc' );
+	$c->get();
+	
+	if( count($c->all) > 0 )
+	{
+		$output .= "<ul>";
+		
+		foreach( $c->all as $item )
+		{
+			$output .= printS( $item->id );
+			$output .= "<li>".add( $id, $item->sort+1 )."</li>";
+		}
+		$output .= "</ul>";
+	}
+	
+	
+	$output .= "</li>";
+	return $output;
+	}
+
+echo printS( 1 );
