c61124a23848cdaa5b4cf9f44d2e1e2af35945ca
Emad Elsaid <blazeeboy@gmail.com>
2009-04-06 18:03:06 +0200

    sepatare docs from the main program
    Signed-off-by: Emad Elsaid <blazeeboy@gmail.com>

--- doc/dojo_manual/_static/dojo/dojo/nls/dojo_ROOT.js
+++
@@ -1,1 +0,0 @@
-dojo.provide("dojo.nls.dojo_ROOT");dojo.provide("dijit.nls.loading");dijit.nls.loading._built=true;dojo.provide("dijit.nls.loading.ROOT");dijit.nls.loading.ROOT={"loadingState":"Loading...","errorState":"Sorry, an error occurred"};
