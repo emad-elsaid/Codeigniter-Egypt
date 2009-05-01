132ec037d3e17f65c70042d4e7b5aace387b2beb
Emad Elsaid <blazeeboy@gmail.com>
2009-05-01 21:12:58 +0300

    content plugin template added

---
+++ system/application/views/content/template.php
@@ -0,0 +1,16 @@
+<?php if( $mode=='config' ){
+//the plugin requirements as a JSON object is here ?>
+{
+	
+}
+
+
+<?php }else if( $mode=='layout' ){ 
+//replace 0 with number of cells your plugin has ?>
+0
+
+
+<?php }else if( $mode=='view' ){
+//the real content of your plugin goes here ?>
+
+<?php } ?>
