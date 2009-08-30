f25e4d7d62a3f82834f169d0500a6e3e7dade31a
Emad Elsaid <blazeeboy@gmail.com>
2009-08-30 17:59:32 +0200

    new toolbar and 960.gs liquid encluded plus content

---
+++ system/application/views/content/960 grid system/box.php
@@ -0,0 +1,42 @@
+<?php if( $mode=='config' ){
+//the plugin requirements as a JSON object is here ?>
+{
+	"title":{"type":"textbox"},
+	"enable_text":{"type":"checkbox"},
+	"text":{"type":"smalleditor"},
+	"enable_cell":{"type":"checkbox"}
+}
+
+
+<?php }else if( $mode=='layout' ){ 
+//replace 0 with number of cells your plugin has ?>
+<?php 
+	if( $info->enable_cell ) echo "1";
+	else echo "0";
+?>
+
+
+<?php }else if( $mode=='view' ){
+//the real content of your plugin goes here ?>
+<?php 
+add( 'assets/960.gs/reset.css');
+add( 'assets/960.gs/text.css');
+//add( 'assets/960.gs/960.css');
+add( 'assets/960.gs/layout.css');
+//add( 'assets/960.gs/nav.css');
+add( 'jquery/jquery.js' );
+//add( 'jquery/jquery-ui.js' );
+//add( 'assets/960.gs/jquery-fluid16.js' );
+add( '<!--[if IE 6]><link rel="stylesheet" type="text/css" href="'.base_url().'assets/960.gs/ie6.css" media="screen" /><![endif]-->');
+add('<!--[if IE 7]><link rel="stylesheet" type="text/css" href="'.base_url().'assets/960.gs/ie.css" media="screen" /><![endif]-->');
+?>
+
+<div class="box">
+	<h2><?= $info->title ?></h2>
+	<div class="block" >
+		<?php if( $info->enable_text ) echo '<p>'.$info->text.'</p>'; ?>
+		<?php if( $info->enable_cell ) echo $cell[0]; ?>
+	</div>
+</div>
+
+<?php } ?>
--- system/application/views/edit_mode/toolbar.php
+++ system/application/views/edit_mode/toolbar.php
@@ -1,138 +1,80 @@
 <?php
 $ci =& get_instance();
-add_js('dojo/dojo/dojo.js');
-add_js( 'jquery/jquery.js' );
-add_js( 'jquery/aqFloater.js' );
-$ci->load->library('gui');
+add( 'assets/edit panel/style.css' );
+add('dojo/dojo/dojo.js');
+add( 'jquery/jquery.js' );
 $local = base_url();
 $url = site_url('admin/app').'/';
 $logout = site_url( 'logout' );
 
 $links = array(
-"toggle images"=>"$('img:not(#adminToolBar img)').toggle();",
-"toggle links"=>"$('a:not(#adminToolBar a)').toggle();",
-"toggle tables"=>"$('table:not(#adminToolBar table)').toggle();",
-"toggle vunsy edit buttons"=>"$('.vunsyCtrl').toggle();",
-"toggle edit toolbar"=>"$('#adminToolBar,#showtoolbar').toggle();"
+"toggle images"=>"vunsyToggle('img:not(.trigger img,.panel img)');",
+"toggle links"=>"vunsyToggle('a:not(.trigger,.panel a)');",
+"toggle tables"=>"vunsyToggle('table:not(.trigger table, .panel table)');",
+"toggle vunsy edit buttons"=>"vunsyToggle('.vunsyCtrl').toggle();"
 );
 $links_text = '';
 foreach( $links as $key=>$value )
 {
-	$links_text .= "<li><a href=\"#\" onclick=\"{$value}\" >
+	$links_text .= "<a href=\"#\" onclick=\"{$value}\" >
 	<img 
 		src=\"{$local}images/admin/jquery/{$key}.png\" 
 		title=\"{$key}\"
-		>{$key}</a></li>";
+		>{$key}</a>";
 }
 ?>
-<script type="text/javascript" >
-function closeAdminTitlepane()
-{
-	alert("sdf");
-	$('#adminToolBar,#showtoolbar').toggle();
-	return false;
+
+<script type="text/javascript">
+
+function vunsyButtonToggler(){
+		$(".panel").toggle("fast");
+		$(this).toggleClass("active");
+		return false;
 }
-$("document").ready( function(){
-	$("#adminToolBar").aqFloater({attach: 'nw', duration: 0.1, opacity: 1});
-	$("#showtoolbar").aqFloater({attach: 'ne', duration: 0.1, opacity: 0.8});
-	$('#adminToolBar').hide();
+	
+function vunsyToggle(selector)
+{
+	$(selector).toggle();
+	vunsyButtonToggler();
+}
+
+$(function (){
+	$(".trigger").click(vunsyButtonToggler);
 });
 </script>
-<style>
-.linksTable{
-	width:100%;
-	border-collapse:collapse;
-	border: 0px none;
-	font-size: 12px;
-}
-.linksTable td{
-	width: 25%;
-	vertical-align: text-top;
-}
-.linksTable img, .linksTable a, .linksTable a:visited{
-	border: 0px none;
-	text-decoration: none;
-	color: #000000;
-	white-space: nowrap;
-}
-.linksTable img{
-	padding-bottom: 4px;
-	vertical-align: middle;
-	margin: 2px;
-	margin-left: 5px;
-}
-.linksTable a, .linksTable a:visited{
-	display: block;
-	width: 100%;	
-	border: 1px solid #BFBFBF;
-}
-.linksTable a:hover{
-	border: 1px solid #4D4D4D;
-}
-.linksTable ul{
-	list-style-type: none;
-}
-.linksTable li{
-	margin: 2px;
-	padding: 2px;
-	padding:0px;
-}
-</style>
-<img id="showtoolbar" onclick="$('#adminToolBar,#showtoolbar').toggle();" 
-src="<?= "{$local}images/admin/jquery/show toolbar.png" ?>"
-title="Show edit toolbar" style="z-index:999;" >
 
-<div id="adminToolBar" style="width:100%;z-index:999;font-size: 12px;" >
-<?php 
-$text = <<<EOT
-<table class="linksTable" >
-	<tr>
-		<th>Applications</th>
-		<th>JQuery Buttons</th>
-		<th></th>
-		<th></th>
-	</tr>
-	<tr>
-		<td>
-			<ul>
-				<li><a href="{$local}kfm" target="_blank" >
-					<img src="{$local}images/admin/kfm.png" title="My Computer" /> My computer
-				</a></li>
-				<li><a href="{$url}user manager" target="_blank" >
-					<img src="{$local}images/admin/users.png" title="Users manager" /> User manager
-				</a></li>
-				<li><a href="{$url}section manager" target="_blank" >
-					<img src="{$local}images/admin/section.png" title="Sections manager" /> Sections manager
-				</a></li>
-				<li><a href="{$url}Package manager" target="_blank" >
-					<img src="{$local}images/admin/software.png" title="Package manager" /> Package manager
-				</a></li>
-				<li><a href="{$url}software manager" target="_blank" >
-					<img src="{$local}images/admin/software.png" title="Software manager" /> Software manager
-				</a></li>
-				<li><a href="javascript:admin_editmode_toolbar()" >
-					<img src="{$local}images/admin/editmode.png" title="Editmode toggle" /> Editmode toggle
-				</a></li>
-				<li><a href="{$logout}" >
-					<img src="{$local}images/admin/logout.png" title="Logout" /> Logout
-				</a></li>
-				</ul>
-		</td>
-		<td>
-			<ul>
-				{$links_text}
-			</ul>
-		</td>
-		<td>
-		</td>
-		<td>
-		</td>
-	</tr>
-</table>
-EOT;
-echo $ci->gui->titlepane( 'Admin tools', $text);
-?>
+
+
+<div class="panel">
+<a href="<?="{$local}"?>kfm" target="_blank" >
+	<img src="<?="{$local}"?>images/admin/kfm.png" title="My Computer" /> My computer
+</a>
+<a href="<?="{$url}"?>user manager" target="_blank" >
+	<img src="<?="{$local}"?>images/admin/users.png" title="Users manager" /> User manager
+</a>
+<a href="<?="{$url}"?>section manager" target="_blank" >
+	<img src="<?="{$local}"?>images/admin/section.png" title="Sections manager" /> Sections manager
+</a>
+<a href="<?="{$url}"?>Package manager" target="_blank" >
+	<img src="<?="{$local}"?>images/admin/software.png" title="Package manager" /> Package manager
+</a>
+<a href="<?="{$url}"?>software manager" target="_blank" >
+	<img src="<?="{$local}"?>images/admin/software.png" title="Software manager" /> Software manager
+</a>
+
+<br />
+<?="{$links_text}"?>
+
+<br />
+<a href="javascript:admin_editmode_toolbar()" >
+	<img src="<?="{$local}"?>images/admin/editmode.png" title="Editmode toggle" /> Editmode toggle
+</a>
+<a href="<?="{$logout}"?>" >
+	<img src="<?="{$local}"?>images/admin/logout.png" title="Logout" /> Logout
+</a>
 </div>
+<a class="trigger" href="#">Edit</a>
+
 <script language="javascript" >
 function admin_editmode_toolbar()
 {
@@ -145,4 +87,3 @@ function admin_editmode_toolbar()
 	});
 }
 </script>
-</div>
