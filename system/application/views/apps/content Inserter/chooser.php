6db691170b2e693abf200c8c79103570be61a740
Emad Elsaid <blazeeboy@gmail.com>
2009-09-20 19:31:44 +0200

    Signed-off-by: Emad Elsaid <blazeeboy@gmail.com>

--- system/application/views/apps/content Inserter/chooser.php
+++ system/application/views/apps/content Inserter/chooser.php
@@ -19,21 +19,23 @@ echo $ci->gui->form(
 
 $recycle = new Content();
 $recycle->get_by_parent_content(0);
-//$recycle_array = array();
-foreach ( $recycle->all as $item ) 
+if( count($recycle->all) >0 )
 {
-	$s = new Section();
-	$s->get_by_id( $item->parent_section );
-	$recycle_array->{$item->id} = $item->path.'('.$s->name.')';
+	foreach ( $recycle->all as $item ) 
+	{
+		$s = new Section();
+		$s->get_by_id( $item->parent_section );
+		$recycle_array->{$item->id} = $item->path.'('.$s->name.')';
+	}
+	
+	echo $ci->gui->form(
+			 $ci->app->app_url('restore'), 
+			array(
+		"Or from recycle bin"=>$ci->gui->dropdown( "id", '',$recycle_array )
+		,""=> $ci->gui->button( "", "Restore that content", array("type"=>"submit") )
+			)
+			,''
+			,$hidden
+	);
 }
-
-echo $ci->gui->form(
-		 $ci->app->app_url('restore'), 
-		array(
-	"Or from recycle bin"=>$ci->gui->dropdown( "id", '',$recycle_array )
-	,""=> $ci->gui->button( "", "Restore that content", array("type"=>"submit") )
-		)
-		,''
-		,$hidden
-);
 ?>
