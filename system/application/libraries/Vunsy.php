ff32d80fc136ad208a76b29accb5c3161634a3a5
Emad Elsaid <blazeeboy@gmail.com>
2009-09-20 03:42:18 +0200

    ID field now the first of table

--- system/application/libraries/Vunsy.php
+++ system/application/libraries/Vunsy.php
@@ -138,7 +138,7 @@ class Vunsy {
 		{
 				$CI->dbforge->add_field($tables[$item]);
 				$CI->dbforge->create_table($item);
-				$CI->db->query("ALTER TABLE `".$item."` ADD COLUMN `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`)");
+				$CI->db->query("ALTER TABLE `".$item."` ADD COLUMN `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`)");
 		}
 	
 		$CI->load->library('datamapper');
