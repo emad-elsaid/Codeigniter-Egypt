b1c56c3baefb53f505a3d9bee1c4fb29180f8f3c
Emad Elsaid <blazeeboy@gmail.com>
2009-09-28 22:08:38 +0200

    edit buttons converted into panel

--- system/application/views/apps/content Inserter/deleteConfirm.php
+++
@@ -1,9 +0,0 @@
-<center>
-	<a href="<?= $ci->app->app_url('delete/'.$ci->uri->segment(5) ) ?>" >
-		Delete that widget forever
-	</a>
-	<br />
-	<a href="<?= $ci->app->app_url('recycle/'.$ci->uri->segment(5) ) ?>" >
-		Put that widget in recycle bin
-	</a>
-</center>
--- system/application/views/edit_mode/container.php
+++ system/application/views/edit_mode/container.php
@@ -2,29 +2,6 @@
 $ci =& get_instance();
 
 add( 'assets/admin/edit/edit.css' );
-add( 'jquery/theme/ui.core.css' );
-add( 'jquery/jquery.js' );
-
-add( <<<EOT
-<script language="javascript" >
-$(function(){
-	$('.editCtrl > .editCtrlToggler' ).click(function(){
-		if( $(this).next('.editGroup').is(':hidden')==false )
-			$(this).next('.editGroup').slideUp('fast');
-		else
-		{
-			$('.editGroup').slideUp('fast');
-			$(this).next('.editGroup').slideDown('fast');
-		}
-		
-	});
-	$('.editGroup a>img' ).click(function(){
-		$(this).parent().parent().fadeOut('fast');
-	});
-});
-</script>
-EOT
-);
 
 $p = new Content();
 $p->get_by_id($parent);
@@ -33,41 +10,9 @@ if( $can_edit OR $p->can_addin() OR $can_delete ){
 ?>
 
 <div class="editCtrl vunsyCtrl"  >
-<img class="editCtrlToggler" src="<?=$img_url?>edit show.png" title="<?=substr( $path, strrpos($path,'/')+1,strrpos($path,'.')-strrpos($path,'/')-1 ) ?>" >
-
-<span class="editGroup ui-helper-hidden" >
-<?php if( $can_edit ){ ?>
-	<a class="iframe " href="<?= site_url("admin/app/content Inserter/data/$id") ?>" title="<?=$path?>" >
-	<img src="<?=$img_url?>edit.png" title="Edit <?= $id ?>" >
+	<a class="iframe " href="<?= site_url("admin/app/content Inserter/edit/$id/{$ci->vunsy->section->id}") ?>" title="<?=$path?>" >
+		<img src="<?=$img_url?>edit show.png" title="<?=substr( $path, strrpos($path,'/')+1,strrpos($path,'.')-strrpos($path,'/')-1 ) ?>" >
 	</a>
-	<a class="iframe " href="<?= site_url("admin/app/content Inserter/up/$id") ?>" title="Move up" >
-	<img src="<?=$img_url?>move up.png"  title="Move up"  >
-	</a>
-	<a class="iframe " href="<?= site_url("admin/app/content Inserter/down/$id") ?>" title="Move down" >
-	<img src="<?=$img_url?>move down.png" title="Move down" >
-	</a>
-<?php } ?>
-
-<?php if( $p->can_addin() ){ ?>
-	<a class="iframe" href="<?= site_url("admin/app/content Inserter/index/{$ci->vunsy->section->id}/$parent/$cell/$sort" ) ?>" >
-	<img src="<?=$img_url?>add before.png" title="Add before" >
-	</a>
-	
-	<a class="iframe" href="<?= site_url("admin/app/content Inserter/index/{$ci->vunsy->section->id}/$parent/$cell/".($sort+1)) ?>" >
-	<img src="<?=$img_url?>add after.png" title="Add after" >
-	</a>
-<?php } ?>
-
-<?php if( $can_delete ){ ?>
-	<a class="iframe" href="<?= site_url("admin/app/content Inserter/deleteConfirm/$id") ?>" >
-	<img src="<?=$img_url?>delete.png" title="Delete" >
-	</a>
-<?php } ?>
-	<a class="iframe" href="<?= site_url("admin/app/content Inserter/info/$id") ?>" >
-	<img src="<?=$img_url?>info.png" title="Information" >
-	</a>
-	
-</span>
 </div>
 <?php } ?>
 <?= $text ?>
