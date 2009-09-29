7afa7d860dddeef8472965038114baf00506695c
Emad Elsaid <blazeeboy@gmail.com>
2009-09-30 01:41:02 +0200

    Signed-off-by: Emad Elsaid <blazeeboy@gmail.com>

--- system/application/views/apps/user manager/viewusr.php
+++ system/application/views/apps/user manager/viewusr.php
@@ -6,22 +6,32 @@ $l->get();
 $ci->load->library('gui');
 
 
+
+//getting user levels and make an array of them
 $levels = new userlevel();
 $levels->get();
-$lvls = array();
+$lvls = '';
 foreach( $levels->all as $item )
 {
-	$lvls[ $item->level ] = $item->name;
+	$l = $item->level;
+	$lvls->$l = $item->name;
 }
 
-echo $ci->gui->tooltipbutton("Add user", $ci->gui->form( $ci->app->app_url('addusraction')
-,array(
+if( $lvls=='')
+	$ci->app->add_error( 'you have to create a new user level before starting to add users' );
+else
+	echo $ci->gui->tooltipbutton(
+					"Add user", 
+					$ci->gui->form( $ci->app->app_url('addusraction'),
+						array(
 		'level' => $ci->gui->dropdown( "level","",$lvls),
 		'name' => $ci->gui->textbox( "name" ),
 		'password' => $ci->gui->password( "password" ),
 		'email' => $ci->gui->textbox( "email" ),
 		'' => $ci->gui->button( "submit", "Add user", array( "type"=>"submit" ) )
-)));
+						)
+					)
+				);
 
 if( $l->count() <= 0 )
 	$ci->app->add_info( " No users available" );
