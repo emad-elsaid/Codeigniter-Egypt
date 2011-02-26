41b1862b545612e75209509afa2456f19ca3355d
Emad Elsaid <blazeeboy@gmail.com>
2011-02-27 01:10:18 +0200

    section editor new interface depends on dojo tree

--- application/controllers/sectionEditor.php
+++ application/controllers/sectionEditor.php
@@ -12,139 +12,123 @@ class SectionEditor extends Application {
 		$this->website 	= "http://blazeeboy.blogspot.com";
 		$this->version 	= "0.1";
 
-		$this->pages 			= array('index'=>'Section Editor');
+		$this->pages 	= array(
+							'index'=>'Pages',
+							'add'=>'New Section'
+							);
 
 		$this->load->library('gui');
 	}
 	
+	function index(){
+
+		add('dojo.data.ItemFileReadStore');
+		add('dijit.tree.ForestStoreModel');
+		add('dijit.Tree');
+		
+		$this->print_text('<div dojoType="dojo.data.ItemFileReadStore" url="'.site_url('sectionEditor/queryTree').'" jsId="ordJson"></div>');
+		$this->print_text('<div dojoType="dijit.tree.ForestStoreModel" childrenAttrs="line" rootLabel="Index Page" store="ordJson" jsId="ordModel"></div>');
+		$this->print_text('<div dojoType="dijit.Tree" id="ordTree" model="ordModel">
+		<script type="dojo/method" event="onClick" args="item">
+		if( item.id!=undefined )
+			document.location.href = "'.site_url('sectionEditor/edit').'/"+item.id;	
+		</script>
+		</div>');
+	}
+	
+	function queryTree(){
+		
+		$this->ajax = TRUE;
+		
+		function getTree($parent=NULL){
+			$section = new Section();
+			$section->where('parent_section', $parent)->get();
+			$line = array();
+			foreach( $section as $item ){
+				$children = getTree($item->id);
+				
+				$line[] = count($children)>0
+							? array('id'=>$item->id, 'description'=>$item->name, 'line'=>$children)
+							: array('id'=>$item->id, 'description'=>$item->name);
+			}
+			return $line;
+		}
+		
+		$this->print_text( json_encode(array( 'identifier'=>'id', 'label'=>'description','items'=>getTree(1))) );
+	}
+	
+	function add(){
+		
+		$this->print_text( $this->gui->form(
+			site_url( 'sectionEditor/addaction' )
+			,array(
+					'Parent :'=>$this->gui->section( 'parent_section')
+					,'Name :'=>$this->gui->textbox( 'name' )
+					,'View Permission :'=>$this->gui->permission( 'view' )
+					,''=>$this->gui->button( '', 'Add Section', array('type'=>'submit') )
+			)
+		));
+	}
+	
 	function addaction(){
 		
-		$this->load->library( 'gui' );
+		$sort = new Section();
+		$sort	->where('parent_section',$this->input->post('parent_section'))
+				->select_max('sort')->get();
+		$sort = $sort->sort+1;		
 		
 		$s = new Section();
 		$s->parent_section = $this->input->post( 'parent_section' );
 		$s->name = $this->input->post( 'name' );
-		$s->sort = $this->input->post( 'sort' );
+		$s->sort = $sort;
 		$s->view = $this->input->post( 'view' );
 		$s->save();
 		
 		redirect( 'sectionEditor' );
 	}
 	
-	function delete($id)
-	{
-		$this->load->library( 'gui' );
+	function delete($id){
+		
 		$s = new Section($id);
 		$s->delete();
-		
+				
 		redirect( 'sectionEditor' );
 	}
 	
-	function edit($id)
-	{
+	function edit($id){
+		
 		$this->load->library( 'gui' );
 		
 		$s = new Section($id);
 		
 		$hidden = array( 
-			'id'=>$id,
-			'parent_section'=>$s->parent_section,
-			'sort'=>$s->sort
+			'id'				=>$id,
+			'parent_section'	=>$s->parent_section,
+			'sort'				=>$s->sort
 		);
 		
 		$this->print_text( $this->gui->form(
 			site_url( 'sectionEditor/editaction' )
 			,array(
-					'Name :'=>$this->gui->textbox( 'name', $s->name )
-					,'view'=>$this->gui->permission( 'view', $s->view )
-					,''=>$this->gui->button( '', 'Edit Section', array('type'=>'submit') )
+				'Name :'	=>$this->gui->textbox( 'name', $s->name )
+				,'view'		=>$this->gui->permission( 'view', $s->view )
+				,''			=>$this->gui->button( '', 'Edit Section', array('type'=>'submit') )
+							.anchor('sectionEditor/delete/'.$id,
+							'I want to delete That Section','onclick="return confirm(\'Are You Sure ?\');"')
 			)
 			,''
 			,$hidden
 		));
 	}
 	
-	function editaction()
-	{
+	function editaction(){
 		
-		$s = new Section($this->input->post( 'id' ));
-		$s->name = $this->input->post( 'name' );
-		$s->view = $this->input->post( 'view' );
+		$s 			= new Section($this->input->post( 'id' ));
+		$s->name 	= $this->input->post( 'name' );
+		$s->view 	= $this->input->post( 'view' );
 		$s->save();
 		
 		redirect( 'sectionEditor' );
 	}
 	
-	function index()
-	{
-		$this->load->library( 'gui' );
-		
-		$sections = new Section();
-		add(<<<EOT
-<style>
-ul{
-	margin-left: 50px;
-}
-</style>
-EOT
-		);
-		foreach( $sections as $item ){
-			$item->e = anchor( 'Edit', site_url( 'sectionEditor/edit' ) );
-			$item->d = anchor( 'Delete', site_url( 'sectionEditor/delete' ) );
-		}
-		
-		function addS( $id, $sort , $text='+' ){
-			$ci =& get_instance();
-			$hidden = array( 'parent_section'=>$id, 'sort'=>$sort );
-			return $ci->gui->tooltipbutton(
-					$text
-					,$ci->gui->form(
-							site_url( 'sectionEditor/addaction' )
-							,array(
-									'Name :'=>$ci->gui->textbox( 'name' )
-									,'View :'=>$ci->gui->permission( 'view' )
-									,''=>$ci->gui->button( '', 'Submit', array('type'=>'submit') )
-							)
-							,''
-							,$hidden
-					)
-			);
-		}
-		
-		function printS( $id ){
-			$s = new Section($id);
-			$s->e = anchor( site_url( 'sectionEditor/edit' ).'/'.$s->id, 'Edit' );
-			$s->d = anchor( site_url( 'sectionEditor/delete' ).'/'.$s->id,'Delete' );
-			
-			$output = "<li>";
-			$output .= $s->id .'|';
-			$output .= $s->name;
-			$output .= addS( $id, 0, "Add child" );
-			$output .= '|';
-			$output .= $s->e;
-			$output .= '|';
-			$output .= $s->d;
-			$c = new Section();
-			$c->where( 'parent_section',  $id );
-			$c->order_by( 'sort', 'asc' );
-			$c->get();
-			
-			if( $c->result_count() > 0 ){
-				$output .= "<ul>";
-				
-				foreach( $c as $item ){
-					$output .= printS( $item->id );
-					$output .= "<li>".addS( $id, $item->sort+1 )."</li>";
-				}
-				$output .= "</ul>";
-			}
-			
-			$output .= "</li>";
-			return $output;
-		}
-		
-		// start to print sections from index page with ID = 1
-		$this->print_text( printS(1) );
-		
-	}
 }
