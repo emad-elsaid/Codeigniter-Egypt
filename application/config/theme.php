7321c6a61f2182370d608919128e301db578c7c9
Emad Elsaid <blazeeboy@gmail.com>
2011-04-05 11:58:05 +0200

    a format change and deletion for the unnecessary files

--- application/config/theme.php
+++ application/config/theme.php
@@ -4,7 +4,7 @@ require 'config.php';
 $config['doctype'] 					= 'xhtml1-trans';
 $config['charset'] 					= 'UTF-8';
 $config['site_name'] 				= $config_site_name;
-$config["dojo_style"]				= "claro";
+$config['dojo_style']				= 'claro';
 
 $config['page_title'] 				= '';
 $config['page_title_prefix'] 		= ' &raquo; ';
